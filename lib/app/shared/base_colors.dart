e47184e50bceff6ccc880e1d5415fb6716a7bf4e
renankanu <renankanu@gmail.com>
2021-11-02 09:42:54 -0300

    create theme and colors

---
+++ lib/app/shared/base_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class BaseColors {
+  BaseColors._();
+
+  static const primaryColor = Color(0xFF2697FF);
+  static const secondaryColor = Color(0xFF2A2D3E);
+  static const bgColor = Color(0xFF212332);
+}
---
+++ lib/app/theme/base_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:web_page_docker/app/shared/base_colors.dart';
+
+class BaseTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: BaseColors.primaryColor,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: GoogleFonts.poppinsTextTheme(Theme.of(Get.context!).textTheme)
+          .apply(bodyColor: Colors.white),
+      canvasColor: BaseColors.secondaryColor,
+    );
+  }
+}
